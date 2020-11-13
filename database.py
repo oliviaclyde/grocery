5461282b02b703dbc6ff29489e7afe3a7f499866
oliviaclyde <olivialclyde@gmail.com>
2020-11-12 20:36:41 -0700

    modified file structure

--- database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker 
-
-engine = create_engine('sqlite:///items.db', connect_args={'check_same_thread': False}, echo=True)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
